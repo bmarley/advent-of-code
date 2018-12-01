4bdcae56dd0f00f5bc6cab787c072fd6af90c029
Andre Azzolini <andre@azzolini.io>
2018-12-01 10:54:56 -0600

    Extract utils

--- src/2018/day01.js
+++ src/2018/day01.js
@@ -1,18 +1,17 @@
 import wu from 'wu'
+import { linesToNumbers, sum } from 'src/utils.js'
 
-const numbers = input => input.split('\n').map(Number)
-const sum = arr => arr.reduce((acc, num) => acc + num, 0)
 const addIfMissing = (set, val) => set.has(val) ? false : Boolean(set.add(val))
 
 export const solvePart1 = input => {
-    return sum(numbers(input))
+    return sum(linesToNumbers(input))
 }
 
 export const solvePart2 = input => {
     const seen = new Set()
     let s = 0
 
-    wu.cycle(numbers(input))
+    wu.cycle(linesToNumbers(input))
         .takeWhile(_ => addIfMissing(seen, s))
         .forEach(n => s += n)
 
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const linesToNumbers = input => input.split('\n').map(Number)
+
+export const sum = arr => arr.reduce((acc, num) => acc + num, 0)
