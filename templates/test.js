2e0d64edadaa7a4f6cf3ddc0e737d04510902fe4
Blake Marley <blake.marley12@gmail.com>
2018-12-02 11:17:00 -0600

    Update templates

--- templates/test.js
+++ templates/test.js
@@ -1,10 +1,11 @@
 import { solvePart1, solvePart2 } from './dayXXX.js'
-
-const example = ``
+import { trim } from 'src/utils.js'
 
 describe('YYY day XXX', () => {
     describe('part 1', () => {
         test('solves an example', () => {
+            const example = trim(``)
+
             const answer = solvePart1(example)
             expect(answer).toEqual()
         })
@@ -12,6 +13,8 @@ describe('YYY day XXX', () => {
 
     describe('part 2', () => {
         test('solves an example', () => {
+            const example = trim(``)
+
             const answer = solvePart2(example)
             expect(answer).toEqual()
         })
