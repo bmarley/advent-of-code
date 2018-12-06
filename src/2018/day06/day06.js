7359e9e8f9047cc4f1bcc9b0cd77773443f16b03
Blake Marley <blake.marley12@gmail.com>
2018-12-06 00:31:53 -0600

    Solve 2018 day 6

---
+++ src/2018/day06/day06.js
@@ -0,0 +1,62 @@
+import { range, maxBy, max, min, sum } from 'lodash'
+import { extractNumbers } from 'src/utils.js'
+
+const distance = ([x1, y1], [x2, y2]) => Math.abs(x2 - x1) + Math.abs(y2 - y1)
+
+export const solvePart1 = input => {
+    const points = input.split('\n').map(l => extractNumbers(l))
+    const upperBound = max(maxBy(points, pair => max(pair))) + 1
+
+    const shortestDistance = Array(upperBound)
+        .fill()
+        .map((e, i) => Array(upperBound))
+
+    range(0, upperBound).forEach(x => {
+        range(0, upperBound).forEach(y => {
+            const distances = points.map(pair => distance(pair, [x, y]))
+            const shortest = min(distances)
+
+            if (distances.filter(d => d === shortest).length === 1) {
+                shortestDistance[x][y] = distances.indexOf(shortest)
+            }
+        })
+    })
+
+    const toIgnore = new Set()
+    range(0, upperBound).forEach(n => {
+        toIgnore.add(shortestDistance[0][n])
+        toIgnore.add(shortestDistance[n][0])
+        toIgnore.add(shortestDistance[shortestDistance.length - 1][n])
+        toIgnore.add(shortestDistance[n][shortestDistance.length - 1])
+    })
+
+    const regionSizes = Array(points.length).fill(0)
+    range(0, upperBound).forEach(x => {
+        range(0, upperBound).forEach(y => {
+            const point = shortestDistance[x][y]
+            if (!toIgnore.has(point)) {
+                regionSizes[point]++
+            }
+        })
+    })
+
+    return max(regionSizes)
+}
+
+export const solvePart2 = input => {
+    const points = input.split('\n').map(l => extractNumbers(l))
+    const upperBound = max(maxBy(points, pair => max(pair))) + 1
+
+    let regionSize = 0
+    range(0, upperBound).forEach(x => {
+        range(0, upperBound).forEach(y => {
+            const distances = points.map(pair => distance(pair, [x, y]))
+
+            if (sum(distances) < (process.env.NODE_ENV === 'test' ? 32 : 10000)) {
+                regionSize++
+            }
+        })
+    })
+
+    return regionSize
+}
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,8 @@
 export const linesToNumbers = input => input.split('\n').map(Number)
 
 // Returns Array<Number> of every number found in the line
-export const extractNumbers = str => (str.match(/-?[0-9]+/g) || []).map(Number)
+export const extractNumbers = str =>
+    (str.match(/-?[0-9.,]+/g) || []).map(n => n.replace(/,/g, '')).map(Number)
 
 // Returns true if given Object has some key with a value matching val
 export const hasVal = (obj, val) => Object.values(obj).some(v => v === val)
