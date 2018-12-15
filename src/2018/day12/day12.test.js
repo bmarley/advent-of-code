e4f23339edffd17178ca4edc791cf801dda1c01b
Andre Azzolini <andre@azzolini.io>
2018-12-15 12:57:11 -0600

    Complete 2018 day 12

---
+++ src/2018/day12/day12.test.js
@@ -0,0 +1,35 @@
+import { solvePart1, solvePart2 } from './day12.js'
+import { trim } from 'src/utils.js'
+
+describe('2018 day 12', () => {
+    const example = trim(`
+        initial state: #..#.#..##......###...###
+
+        ...## => #
+        ..#.. => #
+        .#... => #
+        #.#.# => .
+        .#.#. => #
+        .#.## => #
+        .##.. => #
+        .#### => #
+        #.#.# => #
+        #.### => #
+        ##.#. => #
+        ##.## => #
+        ###.. => #
+        ###.# => #
+        ####. => #
+    `)
+
+    // 0: ...#..#.#..##......###...###...........
+    // 1: ...#...#....#.....#..#..#..#...........
+    // 2: ...##..##...##....#..#..#..##..........
+
+    describe('part 1', () => {
+        test('solves an example', () => {
+            const answer = solvePart1(example)
+            expect(answer).toEqual(325)
+        })
+    })
+})
