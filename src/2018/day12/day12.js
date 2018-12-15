e0a750e0eabdce9169f97895e07873d716df90a9
Blake Marley <blake.marley12@gmail.com>
2018-12-15 12:57:11 -0600

    Complete 2018 day 12

---
+++ src/2018/day12/day12.js
@@ -0,0 +1,77 @@
+import { times } from 'lodash'
+
+const nextGen = (state, rules) => {
+    let shift = 0
+
+    const firstPlant = state.indexOf('#')
+    if (firstPlant < 4) {
+        state.unshift(...Array(4 - firstPlant).fill('.'))
+        shift = 4 - firstPlant
+    }
+
+    const lastPlant = state.lastIndexOf('#')
+    if (lastPlant > state.length - 5) {
+        state.push(...Array(lastPlant - (state.length - 5)).fill('.'))
+    }
+
+    let ns = Array(state.length)
+
+    for (let idx = 0; idx < ns.length; idx++) {
+        if (idx < 2 || idx > ns.length - 2) {
+            ns[idx] = '.'
+        } else {
+            const test = state.slice(idx - 2, idx + 3).join('')
+            ns[idx] = rules.includes(test) ? '#' : '.'
+        }
+    }
+
+    return { shift, nextState: ns }
+}
+
+export const solvePart1 = input => {
+    const lines = input.split('\n')
+    const initialState = lines[0].substring('initial state: '.length).split('')
+    const rules = lines
+        .slice(1)
+        .filter(l => !l.endsWith('.'))
+        .map(l => l.substring(0, 5))
+
+    let base = 0
+    let state = initialState
+
+    times(20, generation => {
+        const { shift, nextState } = nextGen(state, rules)
+        base += shift
+        state = nextState
+    })
+
+    const sum = state.reduce((acc, slot, idx) => {
+        return slot === '#' ? acc + (idx - base) : acc
+    }, 0)
+
+    return sum
+}
+
+export const solvePart2 = input => {
+    const lines = input.split('\n')
+    const initialState = lines[0].substring('initial state: '.length).split('')
+    const rules = lines
+        .slice(1)
+        .filter(l => !l.endsWith('.'))
+        .map(l => l.substring(0, 5))
+
+    let base = 0
+    let state = initialState
+
+    times(100, generation => {
+        const { shift, nextState } = nextGen(state, rules)
+        base += shift
+        state = nextState
+    })
+
+    const sum = state.reduce((acc, slot, idx) => {
+        return slot === '#' ? acc + (idx - base) : acc
+    }, 0)
+
+    return sum + (50000000000 - 100) * 91
+}
