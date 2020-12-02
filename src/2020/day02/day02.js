a664d5f024b234d2c9e99d25408185ff014700b3
Blake Marley <blake.marley12@gmail.com>
2020-12-02 00:13:30 -0700

    d2

---
+++ src/2020/day02/day02.js
@@ -0,0 +1,32 @@
+import _ from 'lodash' // eslint-disable-line
+import * as U from '../../utils.js' // eslint-disable-line
+
+export function solvePart1(input) {
+  let count = 0;
+  const passwords = input.split('\n')
+  passwords.forEach(testpw => {
+    let [policy, letter, password] = testpw.split(" ")
+    letter = letter.replace(':', '')
+    const [first, second] = policy.split('-');
+    const regex = new RegExp(`^(?:[^${letter}]*${letter}){${first},${second}}(?![^${letter}]*${letter})`, "g")
+    count+=!!password.match(regex)
+  })
+  return count;
+}
+
+export function solvePart2(input) {
+  let count = 0;
+  const passwords = input.split('\n')
+  passwords.forEach(testpw => {
+    let [policy, letter, password] = testpw.split(" ")
+    letter = letter.replace(':', '')
+    const [first, second] = policy.split('-');
+
+    if (password[first-1] === letter && password[second-1] !== letter ||
+        password[first-1] !== letter && password[second-1] === letter) {
+          count += 1;
+    }
+    
+  })
+  return count;
+}
---
+++ src/2020/day02/day02.test.js
@@ -0,0 +1,20 @@
+import { solvePart1, solvePart2 } from './day02.js' // eslint-disable-line
+import * as U from '../../utils.js' // eslint-disable-line
+
+describe('2020 day 02', () => {
+  const example = U.trim(`1-3 a: abcde
+  1-3 b: cdefg
+  2-9 c: ccccccccc`)
+
+  describe('part 1', () => {
+    test('solves an example', () => {
+      expect(solvePart1(example)).toEqual(2)
+    })
+  })
+
+  describe('part 2', () => {
+    test('solves an example', () => {
+      expect(solvePart2(example)).toEqual(1)
+    })
+  })
+})
