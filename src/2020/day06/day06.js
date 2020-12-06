54bdd3c75e8d4a3011ff5e1b382547add6147a77
Blake Marley <blake.marley12@gmail.com>
2020-12-05 22:42:01 -0700

    d6 cleanup

--- src/2020/day06/day06.js
+++ src/2020/day06/day06.js
@@ -5,7 +5,7 @@ export function solvePart1(input) {
     const groups =input.split('\n\n').map(l => l.replace(/\n/g, ''))
 
     return groups.map((group) => {
-        var answers = group.replace(' ', '').replace('\n', '');
+        var answers = group.replace('\n', '');
         return _.uniq(answers).length
     }).reduce((acc, toSum) => acc + toSum, 0)
 }
@@ -14,7 +14,7 @@ export function solvePart2(input) {
     const groups =input.split('\n\n')
 
     return groups.map((group) => {
-        let answers = group.replace(' ', '').split('\n').map(simp => simp.split(''))
+        let answers = group.split('\n').map(person => person.split(''))
         return _.intersection(...answers).length
     }).reduce((acc, toSum) => acc + toSum, 0)
 }
